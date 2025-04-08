e1a60fedc51b6f9742b6502047d328673327ec7f
Parasilva Tech <solihulhadi141213@gmail.com>
2025-04-08 22:32:01 +0700

    Init : Commit First Time

---
+++ index.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Rumah Sakit Umum El-Syifa Kuningan, Kabupaten Kuningan. Memberikan pelayanan kesehatan yang profesional dan terpercaya.">
+        <meta name="keywords" content="RSU, RS, RSU El-Syifa, RSEl-Syifa, Elsyifa, RS Elsyifa, RS El-Syifa, Kuningan, Kabupaten Kuningan, Kab. Kuningan">
+        <meta name="author" content="RSU El-Syifa Kuningan">
+        <meta name="robots" content="index, follow">
+        <title>RSU EL-Syifa</title>
+
+        <!-- Open Graph -->
+        <meta property="og:title" content="RSU El-Syifa Kuningan">
+        <meta property="og:description" content="Layanan kesehatan profesional dan terpercaya.">
+        <meta property="og:image" content="https://example.com/images/og-image.jpg">
+        <meta property="og:url" content="https://el-syifa-kuningan.id">
+        <meta property="og:type" content="website">
+
+        <!-- Open Graph untuk Social Media (Facebook, LinkedIn, dll) -->
+        <meta property="og:title" content="RSU El-Syifa Kuningan">
+        <meta property="og:description" content="Rumah Sakit Umum El-Syifa Kuningan, Kabupaten Kuningan. Memberikan pelayanan kesehatan yang profesional dan terpercaya.">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://rsuelsyifa.org">
+        <meta property="og:image" content="https://rsuelsyifa.org/assets/img/favicon-32x32.png"> <!-- Ganti jika punya gambar banner OG khusus -->
+
+        <!-- Twitter Card -->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="RSU El-Syifa Kuningan">
+        <meta name="twitter:description" content="Rumah Sakit Umum El-Syifa Kuningan, Kabupaten Kuningan. Memberikan pelayanan kesehatan yang profesional dan terpercaya.">
+        <meta name="twitter:image" content="https://rsuelsyifa.org/assets/img/favicon-32x32.png"> <!-- Ganti jika punya gambar banner Twitter khusus -->
+
+        <!-- Favicon & Icons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="assets/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon-16x16.png">
+        <link rel="manifest" href="assets/img/site.webmanifest">
+        
+        <!-- Canonical URL (SEO untuk mencegah duplikat) -->
+        <link rel="canonical" href="https://rsuelsyifa.org">
+
+        <!-- Bootstrap -->
+        <link href="node_modules\bootstrap\dist\css\bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+        <script src="node_modules\bootstrap\dist\js\bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+
+        <!-- Bootstrap Icon -->
+        <link rel="stylesheet" href="node_modules/bootstrap-icons/font/bootstrap-icons.min.css">
+
+        <!-- Google Fonts Roboto -->
+        <link rel="stylesheet" href="assets/css/font.css?family=Roboto:wght@300;400;500;700;900&display=swap"/>
+
+        <!-- Custome CSS -->
+        <link rel="stylesheet" href="assets/css/custome.css">
+    </head>
+    <body>
+        <!-- HEADER -->
+        <header class="container-fluid py-2 px-3">
+            <div class="d-flex flex-wrap justify-content-between align-items-center">
+                <!-- Logo dan Judul -->
+                <div class="logo">
+                    <img src="assets/img/67718ce144463.png" alt="Logo RSU El-Syifa">
+                    <h4 class="mb-0">
+                        <a href="">RSU El-Syifa</a>
+                    </h4>
+                </div>
+                <!-- Navigasi -->
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="mainNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item"><a class="nav-link" href="#">Beranda</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Profil</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Layanan</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Dokter</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#">Kontak</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </header>
+
+        <!-- MAIN CONTENT -->
+        <main class="container my-5">
+            <!-- Row 1: Konten utama -->
+            <div class="row mb-5">
+            <div class="col-12">
+                <h2>Selamat Datang di RSU El-Syifa Kuningan</h2>
+                <p>Konten utama akan ditampilkan di sini...</p>
+            </div>
+            </div>
+
+            <!-- Row 2: Kontak dan link lainnya -->
+            <div class="row">
+            <!-- Kontak -->
+            <div class="col-md-4">
+                <h5>Kontak Kami</h5>
+                <p>Jl. Contoh No.123, Kuningan</p>
+                <p>Telp: (0232) 123456</p>
+                <p>Email: info@rsuelsyifa.org</p>
+            </div>
+
+            <!-- Menu lainnya -->
+            <div class="col-md-4">
+                <h5>Menu Lainnya</h5>
+                <ul class="list-unstyled">
+                <li><a href="#">Pendaftaran Online</a></li>
+                <li><a href="#">Jadwal Dokter</a></li>
+                <li><a href="#">Artikel Kesehatan</a></li>
+                </ul>
+            </div>
+
+            <!-- Eksternal Link -->
+            <div class="col-md-4">
+                <h5>Link Eksternal</h5>
+                <ul class="list-unstyled">
+                <li><a href="https://kemkes.go.id" target="_blank">Kementerian Kesehatan</a></li>
+                <li><a href="https://bpjs-kesehatan.go.id" target="_blank">BPJS Kesehatan</a></li>
+                </ul>
+            </div>
+            </div>
+        </main>
+
+        <!-- FOOTER -->
+        <footer>
+            <p>&copy; 2025 RSU El-Syifa Kuningan. All rights reserved.</p>
+        </footer>
+    </body>
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.min.js?v=<?php echo date('YmdHis'); ?>"></script>
+    <script>
+        $(document).ready(function(){
+            $('#klik').on('click', function(){
+                alert('Berhasil! jQuery aktif.');
+            });
+        });
+    </script>
+</html>
